ff461fa58346da77c0eaa730255e3e41ec62f85c
BaraniGanesan23 <barani.g23@gmail.com>
2025-04-29 11:58:02 +0530

    assignment_2

---
+++ EdgeBrowser.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser B2 = new Browser();
+		System.out.println(B2.launchBrowser("safari"));
+		B2.loadUrl();
+
+
+	}
+
+}
