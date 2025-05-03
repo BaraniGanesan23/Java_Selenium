fec7f41327d7daef8a1426344df212bae0ad210d
BaraniGanesan23 <barani.g23@gmail.com>
2025-05-03 15:00:33 +0530

    Update Assignment_Palindrome.java

--- day2/Assignment_Palindrome.java
+++ day2/Assignment_Palindrome.java
@@ -2,18 +2,26 @@ package week1.day2;
 
 public class Assignment_Palindrome {
 	
-	void palindrome(String var) {
+	String palindrome(String var) {
 		String res= "";
 		for (int i=4;i>=0; i--) {
-			//System.out.println(charAt(i));
 			res = res + var.charAt(i);
-			System.out.println(res);
 		}
+		return(res);
 	}
 
 	public static void main(String[] args) {
 		Assignment_Palindrome T1 = new Assignment_Palindrome();
-		T1.palindrome("madam");
+		String var = "madam";
+		String r1 = T1.palindrome(var);
+		System.out.println(r1);
+		if (var.equals(r1)) {
+			System.out.println("it is palindrome");
+		}
+		else {
+			System.out.println("not palindrome");
+			
+		}
 
 		}
 	}
